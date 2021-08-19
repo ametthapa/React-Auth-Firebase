bc7e468aded326d2920e6e8d37830d4c783a8370
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-19 22:05:43 +0545

    catch error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Signup from "./components/Signup";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "./components/contexts/AuthContext";
 
 function App() {
   return (
-    <Container
-      classNamr="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
   );
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,18 +1,41 @@
-import { Form, Card, Button } from "react-bootstrap";
+import { Form, Card, Button, Alert } from "react-bootstrap";
 import { useRef } from "react";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "./contexts/AuthContext";
+import { useState } from "react";
 
 const Signup = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+    setLoading(true);
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref={emailRef} required />
@@ -25,7 +48,7 @@ const Signup = () => {
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button type="submit" className="w-100">
+            <Button disabled={loading} type="submit" className="w-100">
               Sign Up
             </Button>
           </Form>
