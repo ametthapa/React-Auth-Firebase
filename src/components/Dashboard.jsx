4e6c480190592daa5cadddb3c8d4962fb1a9181b
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-20 13:51:06 +0545

    private routing
    private routing for redirection

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,33 @@
 import { useState } from "react";
 import { Card, Button, Alert } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "./contexts/AuthContext";
 
 const Dashboard = () => {
   const [error, setError] = useState(false);
-  function handleLogout() {}
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogout() {
+    setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("failed to logout");
+    }
+  }
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
+          <strong>E-mail : </strong>
+          {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
