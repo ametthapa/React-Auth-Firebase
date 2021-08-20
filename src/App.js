324b4a8fea7c391b89fba8a453ee05d29846a290
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-20 12:03:32 +0545

    added login and dashboard

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import "./App.css";
 import Signup from "./components/Signup";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./components/contexts/AuthContext";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <BrowserRouter>
+          <AuthProvider>
+            <Switch>
+              <Route path="/" exact component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </BrowserRouter>
+      </div>
+    </Container>
   );
 }
 
